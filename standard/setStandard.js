d61bf0c83b4bfbcea5df615e342820a58a3b151c
Caleb Sander <caleb.sander@gmail.com>
2014-03-10 19:41:57 -0400

    Working on load function

--- standard/setStandard.js
+++ standard/setStandard.js
@@ -2,21 +2,37 @@ var _rootDir = '/geartickswebsite/standard/';
 var _standards = {
 	'navbar': {
 		position:'prepend body', //either prepend or append, then the element you want to put it in 
-		url: 'navbar.html'
+		url: 'navbar.html',
+		events: [
+			{
+				selector: '#header>img',
+				eventname: 'load',
+				run: function(event) {
+					console.log(event);
+				}
+			}
+		]
 	}
 };
 
 for (var i in _standards) {
 	$.ajax({
 		url: _rootDir + _standards[i].url,
-		success: function(d) {
-			if (_standards[this.i].position[0] == 'p') { //prepend 
-				$(_standards[this.i].position.substr(8)).prepend(d);
+		success: (function() {
+			return function(d) {
+				if (_standards[this.i].position[0] == 'p') { //prepend 
+					$(_standards[this.i].position.substr(8)).prepend(d);
+				}
+				else { //append
+					$(_standards[this.i].position.substr(7)).append(d);
+				}
+				if (standards[this.i].events) {
+					for (var j = 0; j < standards[this.i].events.length; j++) {
+						$(standards[this.i].events[i].selector).on(standards[this.i].events[i].eventname, standards[this.i].events[i].run);
+					}
+				}
 			}
-			else { //append
-				$(_standards[this.i].position.substr(7)).append(d);
-			}
-		},
+		}()),
 		dataType: 'html',
 		i: i
 	});
